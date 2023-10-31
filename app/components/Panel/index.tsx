c9ad5b238fb8a37cdbead3a9e1aa169dde7b69a7
jonlucasoffice <93656097+jonlucasoffice@users.noreply.github.com>
2023-10-31 09:34:49 +0100

    Loading screen

--- app/components/Panel/index.tsx
+++ app/components/Panel/index.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React from "react"
+import React, { useEffect, useState } from "react"
 import Notes from "../Notes"
 
 export default function Panel({
@@ -10,36 +10,48 @@ export default function Panel({
   aq: any
   noteModifiers: any
 }) {
+  const [loaded, setLoaded] = useState(false)
+
+  useEffect(() => {
+    aq.location && setLoaded(true)
+  }, [aq])
+
   return (
     <div
       id="panel"
-      className="absolute bottom-0 left-0 w-full md:w-auto md:m-10 z-10 p-5 pl-0 rounded text-xs uppercase text-center"
+      className="absolute bottom-0 left-0 w-full md:w-auto md:m-10 z-10 p-5 rounded text-xs uppercase text-center"
     >
-      <div>
-        <div className="font-bold">Sound Vane</div>
+      {loaded ? (
         <div>
-          {aq.location ? aq.location.city.name : "Air quality you can hear"}
+          <div>
+            <div className="font-bold">Sound Vane</div>
+            <div>
+              {aq.location ? aq.location.city.name : "Air quality you can hear"}
+            </div>
+          </div>
+          <Notes noteModifiers={noteModifiers} />
+          <div className="flex justify-center gap-12">
+            <div>
+              <div>O3</div>
+              <div>{aq.o3 ? aq.o3 : "N/A"}</div>
+            </div>
+            <div>
+              <div>Humidity</div>
+              <div>{aq.humidity ? aq.humidity : "N/A"}</div>
+            </div>
+            <div>
+              <div>CO</div>
+              <div>{aq.co ? aq.co : "N/A"}</div>
+            </div>
+            <div>
+              <div>PM 2.5</div>
+              <div>{aq.pm25 ? aq.pm25 : "N/A"}</div>
+            </div>
+          </div>
         </div>
-      </div>
-      <Notes noteModifiers={noteModifiers} />
-      <div className="flex justify-center gap-12">
-        <div>
-          <div>O3</div>
-          <div>{aq.o3 ? aq.o3 : "N/A"}</div>
-        </div>
-        <div>
-          <div>Humidity</div>
-          <div>{aq.humidity ? aq.humidity : "N/A"}</div>
-        </div>
-        <div>
-          <div>CO</div>
-          <div>{aq.co ? aq.co : "N/A"}</div>
-        </div>
-        <div>
-          <div>PM 2.5</div>
-          <div>{aq.pm25 ? aq.pm25 : "N/A"}</div>
-        </div>
-      </div>
+      ) : (
+        <div>Loading Air Quality Data…</div>
+      )}
     </div>
   )
 }
