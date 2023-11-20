53571a83067713f72e8564bc4458896bd447f30d
jonlucasoffice <93656097+jonlucasoffice@users.noreply.github.com>
2023-11-20 20:13:32 +0100

    Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import "./globals.css"
+import Script from "next/script"
 
 export const metadata: Metadata = {
   title: "Sound Vane",
@@ -13,6 +14,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-SWDYC13TGW" />
+      <Script id="google-analytics">
+        {`
+         window.dataLayer = window.dataLayer || [];
+         function gtag(){dataLayer.push(arguments);}
+         gtag('js', new Date());
+       
+         gtag('config', 'G-SWDYC13TGW');
+        `}
+      </Script>
       <body>{children}</body>
     </html>
   )
